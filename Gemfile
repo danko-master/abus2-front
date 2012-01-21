0f87c1fe9682cb79f6560b945e0ed075e6b778a4
jeket102 <jeket102@jeket102-laptop.(none)>
2012-01-21 23:53:26 +0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+gem 'pg', '0.12.2'
+gem 'therubyracer', '0.9.9'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails', '1.0.19'
+
+
+
+group :development do
+  gem 'rspec-rails', '2.8.1'
+end
+
+group :test do
+  gem 'rspec-rails', '2.8.1'
+  gem 'webrat', '0.7.3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Abus2::Application.config.secret_token = '5206d67d2018b056be9984854f5477c9d9d56bacf44356578022bda6346596b7534e189686952f288e51a1c4539df17690a878db99f98de8e5ee9ef0251219e3'
